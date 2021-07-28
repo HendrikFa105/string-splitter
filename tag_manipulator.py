293268552ce7e980d2811d7803aee9b528259378
unknown <hendrikfahrenhorst@web.de>
2021-07-28 16:27:22 +0100

    cut 3 - test 2

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -2,7 +2,11 @@ class TagManipulator():
     def parse_string(self, tags):
         result = []
 
-        if len(tags) < 1 :
+
+        if len(tags) < 1 or tags == ",":
             return result
+        
+        result=tags.split(",")
+
 
         return result
